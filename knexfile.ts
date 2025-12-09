493ac54fb2e276a614e3d0e52fcebe3ea9da376c
Mr M <mc.faithful@stu.unizik.edu.ng>
2025-12-09 04:28:44 +0100

    chore: add .env.example

--- knexfile.ts
+++ knexfile.ts
@@ -37,12 +37,13 @@ const config: { [key: string]: Knex.Config } = {
   },
   production: {
     client: "mysql2",
-    connection: {
+    connection: process.env.DATABASE_URL || {
       host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT) || 3306,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      ssl: { rejectUnauthorized: false }, // Required for many cloud DBs
     },
     migrations: {
       directory: "./src/database/migrations",
